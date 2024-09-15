144a4227ac9169602cbececb7577f441caa9e1ba
Vinay chaudhary <vinay.heyworld@gmail.com>
2024-09-15 21:28:23 +0530

    tRPC context: dummy text added in context for testing

--- src/server/context.ts
+++ src/server/context.ts
@@ -1,15 +1,20 @@
 import { currentUser } from "@clerk/nextjs/server";
 import { inferAsyncReturnType } from "@trpc/server";
+import { emit } from "process";
 
 export const createContext = async(req: Request) => {
     const user = await currentUser();
     
-    if (!user) {
-        throw new Error("Unauthorized"); 
-      }
+    // if (!user) {
+    //     throw new Error("Unauthorized"); 
+    //   }
     
-      const id = user.emailAddresses[0]?.id;
-      const email = user?.emailAddresses[0].emailAddress;
+      // const id = user?.emailAddresses[0]?.id;
+      // const email = user?.emailAddresses[0].emailAddress;
+
+      const id = "dummy testing"
+      const email = "dummy testing"
+      
       return { id, email };
 }; 
 
