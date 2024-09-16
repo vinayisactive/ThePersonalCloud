05474a213312bb1592e2d296facb5a376606c08f
Vinay chaudhary <vinay.heyworld@gmail.com>
2024-09-16 11:21:31 +0530

    Uploadthigns: uploadthing tailwind config added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
+import { withUt } from "uploadthing/tw";
 
-const config: Config = {
+const config: Config = withUt({
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -11,5 +12,7 @@ const config: Config = {
     },
   },
   plugins: [],
-};
+});
+
 export default config;
+
