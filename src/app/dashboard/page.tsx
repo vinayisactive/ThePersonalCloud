4d0ff609e185d121f0fcc5049d1fbea1a963ccce
Vinay chaudhary <vinay.heyworld@gmail.com>
2024-09-16 11:23:48 +0530

    Dashboard: testing uplaod things

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,24 @@
+"use client";
+import { UploadButton } from "@/utils/uploadthings";
+
 const page = () => {
   return (
-    <div className=" w-screen h-screen flex justify-center items-center">
-            <h1 className="text-black">Dashboard</h1>
-    </div>
-  )
-}
+    <div className=" w-screen h-screen flex flex-col gap-2 justify-center items-center">
+      <h1 className="text-black">Dashboard</h1>
 
-export default page
+      <UploadButton
+        endpoint="fileUploader"
+        onClientUploadComplete={(res) => {
+          console.log("Files: ", res);
+          alert("Upload Completed");
+        }}
+        onUploadError={(error: Error) => {
+          console.log(error)
+          alert(`ERROR! ${error}`);
+        }}
+      />
+    </div>
+  );
+};
+
+export default page;
