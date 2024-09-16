7d8f546b4aca2a471ab9837340b1d263f1e60ab2
Vinay chaudhary <vinay.heyworld@gmail.com>
2024-09-16 14:54:44 +0530

    Database: Added schemas for notes, images and files

--- src/database/schema.ts
+++ src/database/schema.ts
@@ -1,4 +1,4 @@
-import { pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
+import { pgTable, serial, text, timestamp, integer } from "drizzle-orm/pg-core";
 
 export const user = pgTable("user", {
     id: serial('id').primaryKey(),
@@ -7,6 +7,35 @@ export const user = pgTable("user", {
     createdAt: timestamp('created_at').defaultNow(),
 }); 
 
+export const files = pgTable("files", {
+    id: serial('id').primaryKey(),
+    userId: integer('user_id').references(() => user.id).notNull(),
+    url: text('url').notNull(),
+    name: text('name').notNull(),
+    key: text('key').notNull(),
+    size: integer('size').notNull(),
+    createdAt: timestamp('created_at').defaultNow(),
+});
+
+export const images = pgTable("images", {
+    id: serial('id').primaryKey(),
+    userId: integer('user_id').references(() => user.id).notNull(),
+    url: text('url').notNull(),
+    name: text('name').notNull(),
+    key: text('key').notNull(),
+    size: integer('size').notNull(),
+    createdAt: timestamp('created_at').defaultNow(),
+});
+
+export const notes = pgTable("notes", {
+    id: serial('id').primaryKey(),
+    userId: integer('user_id').references(() => user.id).notNull(),
+    note: text('note').notNull(),
+    createdAt: timestamp('created_at').defaultNow(),
+});
 
 
-export type User = typeof user.$inferSelect; 
+export type User = typeof user.$inferSelect;
+export type File = typeof files.$inferSelect;
+export type Image = typeof images.$inferSelect;
+export type Note = typeof notes.$inferSelect;
