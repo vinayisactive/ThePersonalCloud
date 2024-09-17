2ae02c4ada49534ed8cda31570e135b84029033f
Vinay chaudhary <vinay.heyworld@gmail.com>
2024-09-17 16:07:43 +0530

    Providers: added tRPC provider in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({ children,}: Readonly<{ children: React.Reac
         <Navbar />
         <Provider>
         <NextSSRPlugin  routerConfig={extractRouterConfig(ourFileRouter)} />
-        {children}
+           {children}
         </Provider>
       </body>
     </html>
