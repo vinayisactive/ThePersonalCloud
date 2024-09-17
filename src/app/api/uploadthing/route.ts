dc1ad36f458125a1e9e02010c7b7afbcc229b498
Vinay chaudhary <vinay.heyworld@gmail.com>
2024-09-17 18:25:32 +0530

    Testing: man i don't know tbh

--- src/app/api/uploadthing/route.ts
+++ src/app/api/uploadthing/route.ts
@@ -3,8 +3,4 @@ import { ourFileRouter } from "./core";
 
 export const { GET, POST } = createRouteHandler({
   router: ourFileRouter,
-  config: {
-    uploadthingId: process.env.UPLOADTHING_APP_ID,
-    uploadthingSecret: process.env.UPLOADTHING_SECRET,
-  } as any
 });
