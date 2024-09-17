0279b4b55a21d6400144d88fd0c83ad8aaacdd1c
Vinay chaudhary <vinay.heyworld@gmail.com>
2024-09-17 16:06:48 +0530

    Dashboard: added components in dashbaord for management

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,34 +1,33 @@
-"use client";
-import { UploadButton } from "@/utils/uploadthings";
+import Account from "@/components/Account";
+import Cloud from "@/components/Cloud";
+import Notes from "@/components/Notes";
+import Photos from "@/components/Photos";
 
 const page = () => {
   return (
-    <div className=" w-screen h-screen flex flex-col gap-2 justify-center items-center">
-      <h1 className="text-black">Dashboard</h1>
+    <div className="w-screen h-screen flex gap-2 justify-center items-center px-4 lg:px-40">
+        <div className="w-full h-screen lg:h-auto grid grid-cols-1 gap-4 lg:grid-cols-3 mt-36 lg:mt-0">
+          
+          <div className="border rounded-3xl h-[300px] w-full flex justify-center items-center">
+            <Account />
+          </div>
 
-      <UploadButton
-        endpoint="imageUploader"
-        onClientUploadComplete={(res) => {
-          console.log("Files: ", res);
-        }}
-        onUploadError={(error: Error) => {
-          console.log(error)
-          alert(`ERROR! ${error}`);
-        }}
-      />
+          <div className="border rounded-3xl h-[300px] w-full col-span-1 lg:col-span-2  flex justify-center items-center">
+              <Photos/>
+          </div>
 
-     <UploadButton
-        endpoint="fileUploader"
-        onClientUploadComplete={(res) => {
-          console.log("Files: ", res);
-        }}
-        onUploadError={(error: Error) => {
-          console.log(error)
-          alert(`ERROR! ${error}`);
-        }}
-      />
-    </div>
+          <div className="border rounded-3xl h-[300px] w-full col-span-1 lg:col-span-2  flex justify-center items-center">
+            <Cloud />
+          </div>
+
+          <div className="border rounded-3xl h-[300px] w-full flex justify-center items-center">
+            <Notes/>
+          </div>
+
+        </div>
+      </div>
   );
 };
 
 export default page;
+
